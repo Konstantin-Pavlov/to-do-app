56223d1af15f28bc99d2245c468370a1a8a2001d
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-12-31 22:28:29 +0600

    add method for writing to json

--- src/main/java/util/JsonReader.java
+++ src/main/java/util/JsonReader.java
@@ -2,28 +2,51 @@ package util;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import model.Task;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class JsonReader {
+    private static GsonBuilder gsonBuilder = new GsonBuilder().registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter());
+    private static Gson gson = gsonBuilder.setPrettyPrinting().create();
+
+    private static final String path = "data/tasks.json";
+
     private JsonReader() {
     }
-    public static <T> T[] loadJsonArray(String path, Class<T[]> cls) {
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter());
-        Gson gson = gsonBuilder.setPrettyPrinting().create();
+
+    public static List<Task> loadJsonArray() {
+
         Path PATH = Paths.get(path);
         try {
             String str = Files.readString(PATH);
-            return gson.fromJson(str, cls);
+            return gson.fromJson(str, new TypeToken<List<Task>>() {
+            }.getType());
         } catch (IOException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
             return null;
         }
     }
+
+    public static void writeFile(List<Task> tasks) {
+        Path PATH = Paths.get(path);
+        String json = gson.toJson(tasks);
+
+        byte[] bytes = json.getBytes();
+        try {
+            Files.write(PATH, bytes);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+
 }
