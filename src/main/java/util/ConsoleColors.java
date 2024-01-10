0304e61f10bb5f4ec0539c7b2d485157a3cc9e88
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-01-10 13:18:34 +0600

    add class contains console colors

---
+++ src/main/java/util/ConsoleColors.java
@@ -0,0 +1,7 @@
+package util;
+
+public class ConsoleColors {
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
+}
