979bfb62352dd2225aab1dceca8edc3e18e060ce
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-01-10 13:16:34 +0600

    add new methods

--- src/main/java/model/Task.java
+++ src/main/java/model/Task.java
@@ -1,22 +1,19 @@
 package model;
 
-import com.google.gson.annotations.SerializedName;
+import exceptions.GradeException;
+import exceptions.TaskException;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Task {
-    private String title; //название
+    private final String title; //название
     private String description; // описание
-    @SerializedName("createdDate")
-    private LocalDateTime createdDate; //дата создания
-    @SerializedName("completionDate")
-    private LocalDateTime completionDate; //дата завершения
-    private Priority priority;
+    private final LocalDateTime createdDate; //дата создания
+    private final LocalDateTime completionDate; //дата завершения
+    private final Priority priority;
     private Status status;
-
-    public Task() {
-    }
+    private Grade grade;
 
     public Task(String title, String description, LocalDateTime createdDate,
                 LocalDateTime completionDate, Priority priority) {
@@ -25,39 +22,104 @@ public class Task {
         this.completionDate = completionDate;
         this.createdDate = createdDate;
         this.priority = priority;
+        this.grade = Grade.NOT_SET;
         setStatus(Status.NEW);
     }
 
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setPriority(Priority priority) {
-        this.priority = priority;
+    public String getTitle() {
+        return title;
     }
 
     public Priority getPriority() {
         return priority;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void changeGrade(Grade grade) {
+        try {
+            this.grade.setGrade(this, grade.name());
+            this.grade = grade;
+        } catch (GradeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void changeDescription(String newDescription) {
+        try {
+            status.changeDescription(this);
+            setDescription(newDescription);
+        } catch (TaskException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void changeStatusToNew() {
+        try {
+            status.changeToNew(this);
+        } catch (TaskException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void changeStatusToInProgress() {
+        try {
+            status.changeToInProgress(this);
+        } catch (TaskException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void changeStatusToDone() {
+        try {
+            status.changeToDone(this);
+        } catch (TaskException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public boolean deleteTask() {
+        try {
+            status.deleteTask();
+            return true;
+        } catch (TaskException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    private void setDescription(String description) {
+        this.description = description;
+    }
+
     @Override
     public String toString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy MM dd HH:mm");
-        return  String.format("%-20s", "title: ")            + title + "\n" +
-                String.format("%-20s", "description: ")      + description + "\n" +
-                String.format("%-20s", "created Date: ")     + createdDate.format(formatter) + "\n" +
-                String.format("%-20s", "completion Date: ")  + completionDate.format(formatter) + "\n" +
-                String.format("%-20s", "priority: ")         + priority + "\n" +
-                String.format("%-20s", "status: ")           + status + "\n";
+        return String.format("%-20s", "title: ") + title + "\n" +
+                String.format("%-20s", "description: ") + description + "\n" +
+                String.format("%-20s", "created Date: ") + createdDate.format(formatter) + "\n" +
+                String.format("%-20s", "completion Date: ") + completionDate.format(formatter) + "\n" +
+                String.format("%-20s", "priority: ") + priority + "\n" +
+                String.format("%-20s", "status: ") + status + "\n" +
+                String.format("%-20s", "grade: ") + grade + "\n";
     }
-
 
 }
