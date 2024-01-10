72ca179d1ef8e5fc471703c8db4f2987a4d70fbc
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-01-10 13:49:32 +0600

    upd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,28 @@
-import model.Priority;
-import model.Task;
 import service.TaskManager;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-
+// todo - remove repeating tasks
 public class Main {
     public static void main(String[] args) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm");
-        LocalDateTime creationDate = LocalDateTime.parse("2023:12:25 16:00", formatter);
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm");
+//        LocalDateTime creationDate = LocalDateTime.parse("2023:12:25 16:00", formatter);
 
         TaskManager taskManager = new TaskManager();
-        List<Task> tasks = taskManager.getTasks();
+        taskManager.start();
+        taskManager.close();
 
-        System.out.println("#".repeat(42) + "search by priority" + "#".repeat(42));
-        tasks.stream().filter(task -> task.getPriority() == Priority.MEDIUM).forEach(System.out::println); // by priority
-        System.out.println();
+//        List<Task> tasks = taskManager.getTasks();
+//
+//        System.out.println("#".repeat(42) + "search by priority" + "#".repeat(42));
+//        tasks.stream().filter(task -> task.getPriority() == Priority.MEDIUM).forEach(System.out::println); // by priority
+//        System.out.println();
+//
+//        System.out.println("#".repeat(42) + "search by creation date" + "#".repeat(42));
+//        tasks.stream().filter(task -> task.getCreatedDate().equals(creationDate)).forEach(System.out::println); // by creation date
+//        System.out.println();
+//
+//        System.out.println("#".repeat(42) + "search by title key word" + "#".repeat(42));
+//        tasks.stream().filter(task -> task.getTitle().contains("relax")).forEach(System.out::println); // by title key word
 
-        System.out.println("#".repeat(42) + "search by creation date" + "#".repeat(42));
-        tasks.stream().filter(task -> task.getCreatedDate().equals(creationDate)).forEach(System.out::println); // by creation date
-        System.out.println();
-
-        System.out.println("#".repeat(42) + "search by title key word" + "#".repeat(42));
-        tasks.stream().filter(task -> task.getTitle().contains("relax")).forEach(System.out::println); // by title key word
-//        taskManager.start();
-//        taskManager.close();
 
 //        tasks.forEach(Task::changeStatusToInProgress);
 //        tasks.forEach(Task::changeStatusToDone);
