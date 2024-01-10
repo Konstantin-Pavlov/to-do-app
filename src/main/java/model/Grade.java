7e57102d80116f00e418adf35b4dc8bbd7c51a64
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-01-10 13:23:14 +0600

    add enum represents grade

---
+++ src/main/java/model/Grade.java
@@ -0,0 +1,25 @@
+package model;
+
+import exceptions.GradeException;
+
+import static util.ConsoleColors.*;
+
+public enum Grade {
+    NOT_SET,
+    EXCELLENT,
+    GOOD,
+    AVERAGE,
+    BELOW_AVERAGE,
+    FAIL;
+
+
+    public void setGrade(Task task, String gradeName) throws GradeException {
+        if (task.getStatus() != Status.DONE) {
+            throw new GradeException(ANSI_RED + "an attempt to set a task grade when the task status is not 'done'" + ANSI_RESET);
+        }
+        if (task.getGrade() != Grade.NOT_SET) {
+            throw new GradeException(ANSI_RED + "an attempt to rate the task for the second time" + ANSI_RESET);
+        }
+        System.out.println(ANSI_GREEN_BACKGROUND + "Успешно изменили оценку на " + gradeName + ANSI_RESET);
+    }
+}
